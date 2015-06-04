5b2a0c428694afb721b62630c0bcc3d2a6b0ce01
mollsju <git@mollsju.com>
2015-06-04 22:29:01 +0200

    New files

---
+++ app/script/d2b.js
@@ -0,0 +1,23 @@
+function initSearch(searchBox) {
+	if(searchBox.value != "Sök på varg för bästa resultat...") return;
+	searchBox.value = "";
+	searchBox.classList.remove("fadedText");
+}
+
+function checkSearchInput(searchBox) {
+	if(searchBox.value == "") {
+		searchBox.value = "Sök på varg för bästa resultat...";
+		searchBox.classList.add("fadedText");
+	}
+}
+
+function searchClick(searchBox, e) {
+	if(e.keyCode == 13) {
+		document.getElementById("searchImg").click();	
+	}
+}
+
+function search() {
+	var searchBox = document.getElementById("searchTextInput");
+	window.location.href='../search.php?s=1&q=' + searchBox.value;
+}
